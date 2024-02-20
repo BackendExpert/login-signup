58b04e2b676c658cdee0218197d793dde01a7d9b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-20 19:02:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const bcryt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const cookieParser = require("cookie-parser")
