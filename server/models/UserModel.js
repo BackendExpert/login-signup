2531bf947cf14ed62de1db8154f33623e3b06b66
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-20 19:12:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,1 +1,11 @@
 const mongoos = require('mongoose')
+
+const UserSchema = new mongoos.Schema({
+    name: String,
+    email: String,
+    password: String,
+    role: {
+        type: String,
+        default: "visitor"
+    }
+})
