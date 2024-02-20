59f11e9d281e3661220a1c2de983b979e9e2c850
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-20 19:33:10 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -8,11 +8,17 @@ const bcryt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
 
+
 // use UserModel from models folder
 const UserModel = require("./models/UserModel")
 
 // make connection between the server and mongoos
 mongoose.connect("mongodb://127.0.0.1:27017/employee");
+
+const app = express()
+app.use(express.json)
+app.use(cors())
+app.use(cookieParser)
 
 // make route for the register
 app.post('/Signup', (req, res) => {
@@ -26,10 +32,7 @@ app.post('/Signup', (req, res) => {
     }).catch(err => res.json(err))
 })
 
-const app = express()
-app.use(express.json)
-app.use(cors())
-app.use(cookieParser)
+
 
 app.listen(3001, () => {
     console.log("Server is Running..!")
