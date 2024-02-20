06ff1320a6a0b8ccc643036a10cbdb3cf253536d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-20 09:49:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignup/Login.tsx
+++ client/src/components/LoginSignup/Login.tsx
@@ -1,8 +1,25 @@
 const Login = () => {
   return (
     <div className="bg-blue-800 w-full h-screen px-[25%]">
-        <div className="bg-white container mx-auto px-40 py-12">
-            Logins
+        <div className="container mx-auto px-16 py-12">
+            <div className="bg-white py-12 px-10 rounded">
+                <h1 className="text-2xl font-semibold text-center pb-4">Login</h1>
+                <hr className="mb-4" />
+
+                <form>
+                    <div className="my-2">
+                        <label htmlFor="email">Email : </label>
+                        <input type="email" className="w-full h-9 border my-2 rounded pl-2" placeholder="User Email" />
+                    </div>
+                    <div className="my-2">
+                        <label htmlFor="password">Password : </label>
+                        <input type="password" className="w-full h-9 border my-2 rounded pl-2" placeholder="User Password" />
+                    </div>       
+                    <div className="">
+                        <button className="w-full h-9 bg-blue-600 text-white font-semibold rounded duration-500 hover:bg-blue-700">Login</button>
+                    </div>             
+                </form>
+            </div>
         </div>
     </div>
   )
