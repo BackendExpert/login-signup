56cbef96eacbd94aaf851f2fac4fbc2832b3fca4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-20 19:08:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
+// database : employee
+// collection (table) : register
+
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const bcryt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
+
+
 
 const app = express()
 app.use(express.json)
