5ef4baade98959ba3ce5b67b14a21043c2bfc5a0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-20 09:37:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/LoginSignup/Login";
+import SignUp from "./components/LoginSignup/SignUp";
+
 export default function App() {
   return (
     <div className="">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Login" element={<Login />} ></Route>
+          <Route path="/Signup" element={<SignUp />} ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
