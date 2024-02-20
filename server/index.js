cb93961546187f4d7d7fe3e9074ee9ff800e4347
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-20 19:06:46 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -4,3 +4,12 @@ const cors = require("cors")
 const bcryt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
+
+const app = express()
+app.use(express.json)
+app.use(cors())
+app.use(cookieParser)
+
+app.listen(3001, () => {
+    console.log("Server is Running..!")
+})
