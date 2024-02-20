03c8e30afd434d58eb90cabbeb10e09cdf6e0881
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-20 19:20:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -12,8 +12,10 @@ const cookieParser = require("cookie-parser")
 const UserModel = require("./models/UserModel")
 
 // make connection between the server and mongoos
-
 mongoose.connect("mongodb://127.0.0.1:27017/employee");
+
+
+
 
 const app = express()
 app.use(express.json)
