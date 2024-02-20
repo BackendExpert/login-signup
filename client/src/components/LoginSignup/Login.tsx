c013e6f55a8e61bb257ef9212a1d86818cd34633
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-20 09:42:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignup/Login.tsx
+++ client/src/components/LoginSignup/Login.tsx
@@ -1,7 +1,7 @@
 const Login = () => {
   return (
-    <div className="bg-blue-800 w-full h-screen">
-        <div className="bg-white container mx-auto px-32 py-12">
+    <div className="bg-blue-800 w-full h-screen px-32">
+        <div className="bg-white container mx-auto px-40 py-12">
             Logins
         </div>
     </div>
